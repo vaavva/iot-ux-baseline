7baad2d097fc08b36564af5512c3d9f8e2086dbf
Valerie Avva <vaavva@microsoft.com>
2019-09-19 17:57:21 -0700

    Have drop-down to choose which device to display showing, and can display data. Still need to have data not show up until asked for

--- src/areas/iotDevices/iotDevices.tsx
+++ src/areas/iotDevices/iotDevices.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import * as classnames from 'classnames/bind';
-import { TextField, RadioField } from '@microsoft/azure-iot-ux-fluent-controls/lib/components/Field';
+import { TextField, RadioField, CheckboxField, ComboField, SelectField } from '@microsoft/azure-iot-ux-fluent-controls/lib/components/Field';
 import { DateField } from '@microsoft/azure-iot-ux-fluent-controls/lib/components/DateTime';
 import { Component } from 'react';
+import { FormOption } from '@microsoft/azure-iot-ux-fluent-controls/lib/Common';
+import { Button } from '@microsoft/azure-iot-ux-fluent-controls'
 
 const cx = classnames.bind(require('./iotDevices.module.scss'));
 
@@ -14,12 +16,16 @@ export default function IotDevices() {
 
     return (
         <div>
+
+            {/* <DeviceChooser /> */}
+            <Select />
+
             {/* <Switch>
                 <Route path={Paths.iotDevices.index} exact component={Root} />
                 <Route path={Paths.iotDevices.mockDevices} component={MockDevices} />
             </Switch> */}
 
-            <DeviceGetter />
+            {/* <DeviceGetter /> */}
             
 
             <TextField 
@@ -59,6 +65,215 @@ export default function IotDevices() {
 //         </ul>
 //     );
 // }
+
+function DeviceCheckboxes(props: { deviceNames: any; }) {
+    const deviceNames = props.deviceNames;
+//     const initialState = {
+//         field : [false, false, false],
+//     }
+//     let counter = 0
+//     const checkBoxes = deviceNames.map((deviceName: React.ReactNode) => {
+//         const nameField = 'checkbox_field_' + deviceName;
+//         // initialState.field[counter] = false;
+        
+//         <div>
+//             <ComboField
+//                 name={nameField}
+//                 value={checkValue[counter]}
+//                 label={nameField}
+//                 onChange={changeCheckValue}
+//                 // name={nameField}
+//                 // value={initialState.field[counter]}
+//                 // label={nameField}
+//                 // onChange={(newValue) => setState({field: newValue})}
+
+//             />
+//             {deviceName}
+//         </div>
+//         counter++;
+//     }
+// );
+
+    const [nameValue, changeNameValue] = React.useState();
+
+    
+    return (
+    <div>
+            <div style={{marginBottom: '20px'}}>
+                Current value:  {
+                    typeof(nameValue) === 'string' ? `'${nameValue}'`
+                        : <pre>{JSON.stringify(nameValue, null, 2)}</pre>
+                }
+            </div>
+        <ComboField
+            name='devicesNames'
+            label='Devices'
+            value={nameValue}
+            onChange={changeNameValue}
+            options={deviceNames.push("All Devices")}
+            placeholder='Choose a device to View'
+        />
+    </div>
+    );
+
+}
+
+function Select() {
+    const [selectValue, changeSelectValue] = React.useState('');
+    const [selectOptions, changeSelectOptions] = React.useState<FormOption[]>([
+        // show a placeholder text initially:
+        { value: '', label: 'Loading…', disabled: true, hidden: true }
+    ]);
+    const [deviceValue, changeDeviceValue] = React.useState('');
+    const [isShown, changeShown] = React.useState(false);
+    
+
+    React.useEffect(() => {
+        // load the actual options asynchronously. In practice, we'd probably call fetch()
+        // to make an HTTP call and call changeSelectOptions() after the promise resolves.
+        const handle = setTimeout(() => {
+            fetch('http://localhost:5000/deviceList/',
+            )
+            .then(res => res.json())
+            .then((data) => {
+                console.log("Device Names")
+                console.log(data)
+                var newSelectOptions = [{value: 'AllDevices', label: "All Devices"}]
+                newSelectOptions = data.map((deviceName: React.ReactNode) => {
+                        return newSelectOptions = newSelectOptions, {value: deviceName, label: deviceName};
+                        
+                });
+                console.log(newSelectOptions)
+                // newSelectOptions = [{value: 'AllDevices', label: "All Devices"}, {value: 'device2', label: "Device 2"}]
+                changeSelectOptions(
+                    newSelectOptions
+                    // // Replace the placeholder text now that we've finished loading:
+                    // { value: '', label: 'Select an option', hidden: true, disabled: true },
+                    
+                    // // actual options:
+                    // { value: 'option1', label: 'Option 1' },
+                    // { value: 'option2', label: 'Option 2' },
+                );
+            })
+            .catch(console.log)
+            
+        }, 2000);
+
+        // return a function that cleans up after this effect (e.g., if the 
+        // component unloads before the options are fetched):
+        return () => clearTimeout(handle);
+    }, [changeSelectOptions]);
+
+
+    function handleClick(event: MouseEvent) {
+        console.log("Clicked!")
+        fetch('http://localhost:5000/deviceTwin/?deviceId=' + selectValue,
+            )
+            .then(res => res.json())
+            .then((data) => {
+                console.log("Device Names")
+                console.log(data)
+                // changeDeviceValue(data)
+                // console.log(deviceValue)
+                // this.setState({deviceNames: data})
+                changeDeviceValue(data);
+                changeShown(true);
+            })
+            .catch(console.log)
+        console.log(isShown)
+      }
+
+    return (
+        <div>
+            <SelectField
+                name='selectField'
+                value={selectValue}
+                onChange={changeSelectValue}
+                label='Select Field'
+                options={selectOptions}
+            />
+            {/* <Button
+                primary
+                onClick={DisplayTwins}
+                attr={{container: {'data-test-hook': 'button1'}}}
+            >
+                Click to Show Device Twins
+            </Button> */}
+            {/* < TwinButton deviceId={selectValue}/> */}
+            <Button onClick={handleClick}>
+                See Device Twin for {selectValue}
+            </Button>
+            <div >
+                <li>Should I be here? {isShown as boolean}</li>
+                <li>Authentication Type: {(deviceValue as unknown as DeviceInfo).authenticationType}</li>
+                <li>Capabilities: {JSON.stringify((deviceValue as unknown as DeviceInfo).capabilities)}</li>
+                <li>Cloud To Device Message Count: {(deviceValue as unknown as DeviceInfo).cloudToDeviceMessageCount}</li>
+                <li>Connection State: {(deviceValue as unknown as DeviceInfo).connectionState}</li>
+                <li>Device Etag: {(deviceValue as unknown as DeviceInfo).deviceEtag}</li>
+                <li>Device ID: {(deviceValue as unknown as DeviceInfo).deviceId}</li>
+                <li>Etag: {(deviceValue as unknown as DeviceInfo).etag}</li>
+                <li>Last Activity Time: {(deviceValue as unknown as DeviceInfo).lastActivityTime}</li>
+                <li>Properties: {JSON.stringify((deviceValue as unknown as DeviceInfo).properties)}</li>
+                <li>Status: {(deviceValue as unknown as DeviceInfo).status}</li>
+                <li>Status Update Time: {(deviceValue as unknown as DeviceInfo).statusUpdateTime}</li>
+                <li>Tags: {JSON.stringify((deviceValue as unknown as DeviceInfo).tags)}</li>
+                <li>Version: {(deviceValue as unknown as DeviceInfo).version}</li>
+            </div>
+
+        </div>
+    );
+}
+
+// class TwinButton extends Component {
+//     constructor(props: Readonly<{}>) {
+//         super(props);
+//         const deviceId = props.deviceId;
+//         this.state = {
+//             isClicked: false,
+//         }
+//     }
+//     handleClick(event: MouseEvent) {
+//         // event.preventDefault();
+//         // alert(event.currentTarget.tagName); // alerts BUTTON
+//         this.setState(state => ({ isClicked: !this.state.isClicked}));
+//         console.log("Clicked!")
+//         // console.log(this.props.deviceId)
+//       }
+      
+//     render() {
+//         return ( 
+//         <Button onClick={this.handleClick}>
+//             See Device Twin for {/* {this.props.deviceId} */}
+//         </Button>
+//         )
+//     }
+// }
+
+
+class DeviceChooser extends Component {
+    state = {
+        deviceNames: []
+    }
+
+    componentDidMount() {
+        console.log("fetching")
+        fetch('http://localhost:5000/deviceList/',
+            )
+            .then(res => res.json())
+            .then((data) => {
+                console.log("Device Names")
+                console.log(data)
+                this.setState({deviceNames: data})
+            })
+            .catch(console.log)
+    }
+
+    render() {
+        return (
+            <DeviceCheckboxes deviceNames={this.state.deviceNames} />
+        )
+    }
+}
 
 
 function DeviceList(props: { devices: any; }) {
@@ -114,7 +329,7 @@ class DeviceGetter extends Component {
 
     componentDidMount() {
         console.log("fetching")
-        fetch('http://localhost:5000/deviceTwins/',
+        fetch('http://localhost:5000/allDeviceTwins/',
             )
             .then(res => res.json())
             .then((data) => {
